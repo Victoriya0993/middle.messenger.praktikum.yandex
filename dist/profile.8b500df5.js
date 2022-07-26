c70f476a81e4964eaf9c4f53e793ff10c4555be0
Victoriya Ryzhkova <victoriyabogonos@gmail.com>
2022-07-26 22:04:51 +0300

    Update after review

--- dist/profile.8b500df5.js
+++
@@ -1,2 +0,0 @@
-JSON.parse('[{"name":"Андрей","message":"Изображение","img":"","time":"10:49","new":2}]');
-//# sourceMappingURL=profile.8b500df5.js.map
--- dist/profile.b8efd2ce.js
+++
@@ -1,2 +0,0 @@
-JSON.parse('[{"name":"Андрей","message":"Изображение","img":"","time":"10:49","new":2}]');
-//# sourceMappingURL=profile.b8efd2ce.js.map
